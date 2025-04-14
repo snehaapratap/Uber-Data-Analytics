b3f985bccc7d061709e998654bf5e816dafb495c
Sneha P Pratap <snehapratap248@gmail.com>
2025-04-14 22:38:59 +0530

    done

---
+++ etl/extract.py
@@ -0,0 +1,20 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    path = 'data/uber_data.csv'
+    response = requests.get(path)
+
+    return pd.read_csv(io.StringIO(response.text), sep=',')
+
+
+@test
+def test_output(output, *args) -> None:
+    assert output is not None, 'The output is undefined'
